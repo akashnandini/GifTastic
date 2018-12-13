4ceec9c30c60ca73b99d29d7df9af85edb872d7a
Nandini Maity <akashnandini@gmail.com>
2018-12-13 13:02:55 -0500

    GifTastic v1.2

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,43 +1,53 @@
+
  // Initial array of movies
  var topics = ["Tiger", "Lion", "Cat", "Dog"];
 
- // displayMovieInfo function re-renders the HTML to display the appropriate content
+ // displayAnimalInfo function re-renders the HTML to display the appropriate content
  function displayAnimalInfo() {
 
    var animal = $(this).attr("data-name");
    //var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=9SnBtHSAb2LMsAcSDqaXyjm1dbr8eGke&q?q=" + animal + "&limit=10";
    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=9SnBtHSAb2LMsAcSDqaXyjm1dbr8eGke&q=" + animal + "&limit=10";
    console.log(queryURL);
-
+   var len; 
    // Creating an AJAX call for the specific movie button being clicked
    $.ajax({
      url: queryURL,
      method: "GET"
    }).then(function(response) {
         console.log(response);
+        //empty animal-view div
         $("#animals-view").empty();
         
         for (var i = 0; i < response.data.length; i++) {
             
-            var rating = response.data[i].rating; 
+            var rating = response.data[i].rating;            
             console.log("rating== "+rating);
-            var title = response.data[i].title;  
+            var title = response.data[i].title;            
+            var div = $('<div class="gifs">');
+            var image = $("<img>").addClass("images"); 
+            var p = $('<p class="rating">Rating: ' + rating + '</p>');
+       
+            div.append(p,image);
+            console.log("response.data[i].images.fixed_height_still.url=="+response.data[i].images.fixed_height_still.url);
+            console.log("response.data[i].images.fixed_height.url=="+response.data[i].images.fixed_height.url);
+            //  div.append(image);
+            image.attr("src", response.data[i].images.fixed_height_still.url);            
+            image.attr("data-still", response.data[i].images.fixed_height_still.url);
+            image.attr("data-animate", response.data[i].images.fixed_height.url);
+            image.attr("data-state", response.data[i].images.fixed_height_still.url);
             
-            var $div = $('<div class="gifs">');
-            var $image = $('<img>').addClass('images'); 
-            var $p = $('<p class="rating">Rating: ' + rating + '</p>');
             
-            $div.prepend( $p,$image);
-            $image.attr('src', response.data[i].images.fixed_height_still.url) 
-                .attr('data-still', response.data[i].images.fixed_height_still.url)
-                .attr('data-animate', response.data[i].images.fixed_height.url);
-            $("#animals-view").prepend($div) ;
+            $("#animals-view").prepend(div) ;
+          
         }
    });
-}
+ }
+
 
 function animate(){
     var animate = $(this).attr("data-state");
+    console.log(animate);
     if(animate==="still"){
         $(this).attr("src",$(this).attr("data-animate"));
         $(this).attr("data-state","animate");
@@ -48,7 +58,7 @@ function animate(){
       }
 }
 
-// Function for displaying movie data
+// Function for displaying animal data
 function renderButtons() {
 
     $("#buttons-view").empty();
@@ -80,8 +90,11 @@ function renderButtons() {
   });
 
   // Adding a click event listener to all elements with a class of "animal-btn"
+  //$(document).ready(function() {
   $(document).on("click", ".animal-btn", displayAnimalInfo);
+  //$(".images").on("click", function() {
   $(document).on("click", ".images", animate);
+ // $(".animal-btn").on("click",)
 
   // Calling the renderButtons function to display the intial buttons
   renderButtons();
